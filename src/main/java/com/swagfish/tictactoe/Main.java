223f3de857d13b341ee3658b95da6f1ff00b8f26
root <dadi15@ru.is>
2016-10-23 22:09:28 +0000

    web fix

--- src/main/java/com/swagfish/tictactoe/Main.java
+++ src/main/java/com/swagfish/tictactoe/Main.java
@@ -1,9 +1,27 @@
 package com.swagfish.tictactoe;
 
 import static spark.Spark.*;
+import spark.*;
+import spark.servlet.SparkApplication;
 
-public class Main {
+
+public class Main implements SparkApplication {
     public static void main(String[] args) {
-        get("/hello", (req, res) -> "Hello World");
+
+        staticFileLocation("/public");
+        SparkApplication web = new Main();
+
+        String port = System.getenv("PORT");
+        if (port != null) {
+                port(Integer.valueOf(port));
+        }
+
+        web.init();
+
     }
+        @Override
+        public void init()
+        {
+                get("/hello", (req, res) -> "Hello World");
+        }
 }
