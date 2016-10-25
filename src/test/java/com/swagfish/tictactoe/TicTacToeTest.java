a3712e4e769b08009ac8385c9741b66f5646d6bd
JonSteinn <jonsteinn@gmail.com>
2016-10-25 20:01:31 +0000

    more tests

--- src/test/java/com/swagfish/tictactoe/TicTacToeTest.java
+++ src/test/java/com/swagfish/tictactoe/TicTacToeTest.java
@@ -14,19 +14,18 @@ public class TicTacToeTest
 	{
 		assertEquals(3, new TicTacToe().getSize());
 	}
-
 	@Test
 	public void sizeTest2() 
 	{
 		assertEquals(12, new TicTacToe(12).getSize());
 	}
 
+
 	@Test
 	public void NextToMoveTest1()
 	{
 		assertEquals(1, new TicTacToe().getNextToMove());
 	}
-
 	@Test
 	public void NextToMoveTest2()
 	{
@@ -34,6 +33,43 @@ public class TicTacToeTest
 		ttt.add(1, 1);
 		assertEquals(-1, ttt.getNextToMove());
 	}
+	@Test
+	public void NextToMoveTest3()
+	{
+		TicTacToe ttt = new TicTacToe();
+		for (int i = 0; i < 2; i++) { 
+			for (int j = 0; j < 3; j += 2) {
+				ttt.add(i, j);
+			}
+		}
+		assertEquals(1, ttt.getNextToMove());
+	}
+	@Test
+	public void NextToMoveTest4()
+	{
+		TicTacToe ttt = new TicTacToe(10);
+		for (int i = 0; i < 10; i++) ttt.add(0, i);
+		assertEquals(1, ttt.getNextToMove());
+	}
+	@Test
+	public void NextToMoveTest5()
+	{
+		TicTacToe ttt = new TicTacToe(10);
+		for (int i = 0; i < 10; i++) ttt.add(0, i);
+		assertEquals(1, ttt.getNextToMove());
+	}
+	@Test
+	public void NextToMoveTest6()
+	{
+		TicTacToe ttt = new TicTacToe(10);
+		for (int i = 0; i < 10; i++) {
+			ttt.add(0, i);
+			ttt.add(9, i);
+		}
+		ttt.add(4, 4);
+		assertEquals(-1, ttt.getNextToMove());
+	}
+
 
 	@Test
 	public void isOverTest1()
@@ -64,6 +100,116 @@ public class TicTacToeTest
 		}
 		assertEquals(true, ttt.isOver());
 	}
+	@Test
+	public void isOverTest4()
+	{
+		TicTacToe ttt = new TicTacToe();
+		ttt.add(1, 0);
+		ttt.add(0, 1);
+		ttt.add(1, 1);
+		ttt.add(0, 2);
+		ttt.add(1, 2);
+		assertEquals(true, ttt.isOver());
+	}
+	@Test
+	public void isOverTest5()
+	{
+		TicTacToe ttt = new TicTacToe();
+		ttt.add(0, 0);
+		ttt.add(0, 1);
+		ttt.add(1, 1);
+		ttt.add(1, 0);
+		ttt.add(2, 2);
+		assertEquals(true, ttt.isOver());
+	}
+	@Test
+	public void isOverTest6()
+	{
+		TicTacToe ttt = new TicTacToe();
+		ttt.add(2, 0);
+		ttt.add(0, 0);
+		ttt.add(1, 1);
+		ttt.add(0, 1);
+		ttt.add(0, 2);
+		assertEquals(true, ttt.isOver());
+	}
+	@Test
+	public void isOverTest7()
+	{
+		TicTacToe ttt = new TicTacToe(10);
+		for (int i = 0; i < 10; i++) 
+		{
+			for (int j = 0; j < 10; j++) {
+				ttt.add(i, j);
+			}
+		}
+		assertEquals(true, ttt.isOver());
+	}
+	@Test
+	public void isOverTest8()
+	{
+		TicTacToe ttt = new TicTacToe(10);
+		for (int i = 0; i < 10; i += 2) 
+		{
+			for (int j = 0; j < 10; j += 2) {
+				ttt.add(i, j);
+			}
+		}
+		assertEquals(false, ttt.isOver());
+	}
+	@Test
+	public void isOverTest9()
+	{
+		TicTacToe ttt = new TicTacToe(10);
+		ttt.add(0, 0);
+		ttt.add(0, 1);
+		ttt.add(1, 1);
+		ttt.add(0, 2);
+		ttt.add(2, 2);
+		ttt.add(0, 3);
+		ttt.add(3, 3);
+		ttt.add(0, 4);
+		ttt.add(4, 4);
+		ttt.add(0, 5);
+		ttt.add(5, 5);
+		ttt.add(0, 6);
+		ttt.add(6, 6);
+		ttt.add(0, 7);
+		ttt.add(7, 7);
+		ttt.add(0, 8);
+		ttt.add(8, 8);
+		ttt.add(0, 9);
+		ttt.add(9, 9);
+		assertEquals(true, ttt.isOver());
+	}
+	@Test
+	public void isOverTest10()
+	{
+		TicTacToe ttt = new TicTacToe(10);
+		ttt.add(9, 0);
+		ttt.add(9, 1);
+		ttt.add(8, 1);
+		ttt.add(9, 2);
+		ttt.add(7, 2);
+		ttt.add(9, 3);
+		ttt.add(6, 3);
+		ttt.add(9, 4);
+		ttt.add(5, 4);
+		ttt.add(9, 5);
+		ttt.add(4, 5);
+		ttt.add(9, 6);
+		ttt.add(3, 6);
+		ttt.add(9, 7);
+		ttt.add(2, 7);
+		ttt.add(9, 8);
+		ttt.add(1, 8);
+		ttt.add(9, 9);
+		ttt.add(0, 9);
+		assertEquals(true, ttt.isOver());
+	}
+
+
+
 
 	@Test
 	public void alternatingPlayersTest()
