b3b99e87a0e3cdec70086ed2132929d043863658
Dadi <dadi15@ru.is>
2016-10-30 23:35:20 +0000

    Added additional selenium tests

--- src/selenium/java/SeleniumTestWrapper.java
+++ src/selenium/java/SeleniumTestWrapper.java
@@ -19,27 +19,27 @@ public abstract class SeleniumTestWrapper
   @BeforeClass
   public static void openBrowser()
   {
-    /*
-    DesiredCapabilities caps = new DesiredCapabilities();
+
+    /*DesiredCapabilities caps = new DesiredCapabilities();
     caps.setJavascriptEnabled(true);
     caps.setCapability(
       PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY,
-      "/phantomjs"
-    );
-    driver = new PhantomJSDriver(caps);*/
-    driver = new PhantomJSDriver();
-    //river = new ChromeDriver();
+      "phantomjs"
+    );*/
+    //driver = new PhantomJSDriver(caps);
+    //driver = new PhantomJSDriver();
+    driver = new ChromeDriver();
 
 
     driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     //For local testing
-  /*  System.getenv("PORT");
+    /*System.getenv("PORT");
         if (port == null) {
             port = "4567";
-        }
-    baseUrl = "http://localhost:" + port;
-    */
+        }*/
+    //baseUrl = "http://localhost:" + port;
+
 
     // Testing the deployed version
     baseUrl = "https://arcane-tundra-47758.herokuapp.com/";
